189faba62025d93b03a12de7b50da662d18da540
Cami Dvorkin <cdvorkin@medallia.com>
2021-04-30 18:47:02 -0300

    Add test users and projects

--- tests/test_api.py
+++ tests/test_api.py
@@ -18,27 +18,84 @@ def client():
     with tempfile.NamedTemporaryFile() as dbf:
         app = create_app(test_db=f"sqlite:///{dbf.name}")
         with app.app_context():
-            from flask_migrate import upgrade as _upgrade
-
-            _upgrade()
+            db.create_all()
         with app.test_client() as test_client:
             yield test_client
         with app.app_context():
             db.drop_all()
 
 
-def test_root(client):
-    response = client.get("/")
-    assert response._status_code == 200
+def read_graph_ql(client, query, exp_response):
+    response = client.post('/graphql', json=query)
+    assert response.status_code == 200
+    assert json.loads(response.data.decode()) == exp_response
 
 
-def test_hello(client):
-    response = client.get("/v1/hello")
-    assert response._status_code == 200
-    assert json.loads(response.data).get("Say") == "Hello"
+def test_get_all_users_empty_db(client):
+    query = {
+        'query': '''
+            {
+                allUsers {
+                    edges {
+                        node {
+                            id
+                        }
+                    }
+                }
+            }'''
+    }
+    response = {"data": {"allUsers": {"edges": []}}}
+    read_graph_ql(client, query, response)
 
 
-def test_echo(client):
-    response = client.post("/v1/hello", json={"message": "Say hi"})
-    assert response._status_code == 200
-    assert json.loads(response.data).get("Echo") == "Say hi"
+def test_show_all_projects_empty_db(client):
+    query = {
+        'query': '''
+            {
+                allProjects {
+                    edges {
+                        node {
+                            id
+                        }
+                    }
+                }
+            }'''
+    }
+    response = {"data": {"allProjects": {"edges": []}}}
+    read_graph_ql(client, query, response)
+
+
+def test_create_a_user(client):
+    query = {
+        'query': '''mutation {
+            mutateUser(email: "pepe@gmail.com", name: "Pepe Suarez") {
+                user {
+                name,
+                email,
+                }
+            }
+        } '''
+    }
+    response = {
+        "data": {
+            "mutateUser": {"user": {"name": "Pepe Suarez", "email": "pepe@gmail.com"}}
+        }
+    }
+    read_graph_ql(client, query, response)
+
+
+# def test_delete_a_user(client):
+
+# def test_root(client):
+#     response = client.get("/")
+#     assert response._status_code == 200
+
+# def test_hello(client):
+#     response = client.get("/v1/hello")
+#     assert response._status_code == 200
+#     assert json.loads(response.data).get("Say") == "Hello"
+
+# def test_echo(client):
+#     response = client.post("/v1/hello", json={"message": "Say hi"})
+#     assert response._status_code == 200
+#     assert json.loads(response.data).get("Echo") == "Say hi"
