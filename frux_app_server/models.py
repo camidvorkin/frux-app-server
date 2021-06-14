e40e102aa6710bd911559cdd48f64d739eca0c9f
Cami Dvorkin <cdvorkin@medallia.com>
2021-06-14 18:03:52 -0300

    Model with association table

--- frux_app_server/models.py
+++ frux_app_server/models.py
@@ -28,11 +28,20 @@ class User(db.Model):  # type:ignore
     project_investments = db.relationship("Investments", back_populates="user")
 
 
+class HashtagProject(db.Model):  # type:ignore
+    __tablename__ = 'hashtag_project'
+    hashtag_id = db.Column(db.Integer, db.ForeignKey('hashtag.id'), primary_key=True)
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'), primary_key=True)
+    hashtag = db.relationship("Hashtag", back_populates="projects")
+    project = db.relationship("Project", back_populates="hashtags")
+
+
 class Hashtag(db.Model):  # type:ignore
     __tablename__ = 'hashtag'
+    __table_args__ = (db.UniqueConstraint('hashtag', name='unique_hashtag'),)
     id = db.Column(db.Integer, primary_key=True)
     hashtag = db.Column(db.String)
-    id_project = db.Column(db.Integer, db.ForeignKey('project.id'))
+    projects = db.relationship("HashtagProject", back_populates="hashtag")
 
 
 class ProjectStage(db.Model):  # type:ignore
@@ -67,6 +76,7 @@ class Project(db.Model):  # type:ignore
     latitude = db.Column(db.String)
     longitude = db.Column(db.String)
     investors = db.relationship("Investments", back_populates="project")
+    hashtags = db.relationship("HashtagProject", back_populates="project")
 
 
 class Admin(db.Model):  # type:ignore
