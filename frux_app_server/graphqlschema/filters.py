5679bbfa6e4ee33b07b9f4368caca8c0e8405f9b
Cami Dvorkin <cdvorkin@medallia.com>
2021-07-01 21:25:00 -0300

    Distance from project

--- frux_app_server/graphqlschema/filters.py
+++ frux_app_server/graphqlschema/filters.py
@@ -1,10 +1,13 @@
 import graphene
 import sqlalchemy
 from graphene_sqlalchemy_filter import FilterableConnectionField, FilterSet
+from sqlalchemy import Float, cast
 
 from frux_app_server.models import AssociationHashtag as AssociationHashtagModel
 from frux_app_server.models import Project as ProjectModel
 from frux_app_server.models import User as UserModel
+
+DISTANCE = 10.0
 
 
 class UserFilter(FilterSet):
@@ -20,6 +23,7 @@ class UserFilter(FilterSet):
 
 class ProjectFilter(FilterSet):
     has_hashtag = graphene.List(graphene.String)
+    is_closer_than = graphene.List(graphene.Float)
 
     class Meta:
         model = ProjectModel
@@ -41,7 +45,26 @@ class ProjectFilter(FilterSet):
             sqlalchemy.and_(ProjectModel.id == hashtag_association.project_id,),
         )
         filter_ = hashtag_association.hashtag.in_(hashtags)
+        print(query, filter_, hashtags)
         return query, filter_
+
+    @classmethod
+    def is_closer_than_filter(
+        self, info, query, location
+    ):  # pylint: disable=unused-argument
+        '''
+        location = [latitude, longitude, distance (optional)]
+        '''
+        project_location = self.aliased(query, ProjectModel)
+        latitude = location[0]
+        longitude = location[1]
+        n = location[2] if len(location) == 3 else DISTANCE
+        return sqlalchemy.and_(
+            cast(project_location.latitude, Float) < latitude + n,
+            cast(project_location.latitude, Float) > latitude - n,
+            cast(project_location.longitude, Float) < longitude + n,
+            cast(project_location.longitude, Float) > longitude - n,
+        )
 
 
 class FruxFilterableConnectionField(FilterableConnectionField):
